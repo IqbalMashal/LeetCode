495264379b9d4f776b4047b41a74db51ef69bd9c
Iqbal Mashal <151959357+IqbalMashal@users.noreply.github.com>
2024-10-15 23:47:52 -0400

    Time: 748 ms (68.93%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,11 +1,9 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 
 
-SELECT r.contest_id,
-       ROUND(COUNT(r.user_id) * 100.0 / 
-            (SELECT COUNT(user_id) FROM users),
-            2) AS percentage
-FROM users u
-JOIN register r ON u.user_id = r.user_id
+SELECT r.contest_id AS contest_id, 
+       ROUND(COUNT(r.user_id) * 100.0 / (SELECT COUNT(user_id) FROM users),2) AS percentage
+FROM register r 
+JOIN users u ON u.user_id = r.user_id
 GROUP BY r.contest_id
-ORDER BY percentage DESC, r.contest_id ASC;
+ORDER BY percentage DESC, contest_id;
