77e1b0797b57dbe626716cc98d4559e9ec365df3
Iqbal Mashal <151959357+IqbalMashal@users.noreply.github.com>
2024-11-04 21:53:39 -0500

    Time: 0 ms (100%), Space: 8 MB (84.76%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count = 0;
+
+        for(auto& jewel : jewels){
+            for (auto& stone : stones){
+                if (jewel == stone){
+                    count++;
+                }
+            }
+        }
+        return count;  
+    }
+};
